724a90ee106ddf716417d1b6e9a30e52aee1592d
arthurhf <arthur.h.f13@gmail.com>
2019-07-13 23:06:49 -0300

    Adicionado tela de login

---
+++ app/src/main/java/padm/ufabc/edu/saci/loginActivity.kt
@@ -0,0 +1,22 @@
+package padm.ufabc.edu.saci
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+class loginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
