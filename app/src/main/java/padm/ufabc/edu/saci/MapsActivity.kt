4929f567f416a7c3d0955beb7fa4ca134763bbb4
Victor Leite <victor_leite@outlook.com.br>
2019-07-14 12:23:10 -0300

    Configurado e implementado activity com mapa

--- app/src/main/java/padm/ufabc/edu/saci/MapsActivity.kt
+++ app/src/main/java/padm/ufabc/edu/saci/MapsActivity.kt
@@ -1,7 +1,14 @@
 package padm.ufabc.edu.saci
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,9 +21,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private val TAG = "MapActitvity"
+    private val FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
+    private val COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1234
+    private var mPermissionsLocationGranted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        getLocationPermission()
+    }
+
+    private fun initMap() {
+        Log.d(TAG, "initMap: initializing map")
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -33,11 +51,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "The map is ready")
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun getLocationPermission() {
+        Log.d(TAG, "getLocationPermission: getting location permission")
+        val permissions = arrayOf(FINE_LOCATION, COARSE_LOCATION)
+
+        if(ContextCompat.checkSelfPermission(this.applicationContext, FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this.applicationContext, COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mPermissionsLocationGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        Log.d(TAG, "onRequestPermissionsResult: called")
+        mPermissionsLocationGranted = false
+
+        when(requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if(grantResults.size > 0) {
+                    for(result in grantResults) {
+                        if(result != PackageManager.PERMISSION_GRANTED) {
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed")
+                            mPermissionsLocationGranted = false
+                            return
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permisson granted")
+                    mPermissionsLocationGranted = true
+                    // initialize map
+                    initMap()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/padm/ufabc/edu/saci/initialActivity.kt
+++ app/src/main/java/padm/ufabc/edu/saci/initialActivity.kt
@@ -1,18 +1,60 @@
 package padm.ufabc.edu.saci
 
+import android.content.Intent
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 
 import kotlinx.android.synthetic.main.activity_initial.*
 
 class initialActivity : AppCompatActivity() {
 
+    val TAG = "InitialActivity"
+    val ERROR_DIALOG_REQUEST = 9001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial)
-        setSupportActionBar(toolbar)
+        //setSupportActionBar(toolbar)
 
+        if(isServicesOk()) {
+            init()
+        }
+
+    }
+
+    private fun init() {
+        val btnOpenMap = findViewById<Button>(R.id.open_map_button)
+        btnOpenMap.setOnClickListener(View.OnClickListener {
+            @Override
+            fun OnClickAction(view: View) {
+                startActivity(Intent(this@initialActivity, MapsActivity::class.java))
+            }
+        })
+    }
+
+    public fun isServicesOk(): Boolean {
+        Log.d(TAG, "isServicesOK: Checking google services version")
+        var availability = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        if(availability == ConnectionResult.SUCCESS) {
+            // Tudo esta ok. Usuario pode usar maps
+            Log.d(TAG, "isServicesOK: Google play services is working")
+            return true
+        } else if(GoogleApiAvailability.getInstance().isUserResolvableError(availability)) {
+            Log.d(TAG, "isServicesOK: an error ocurred but we can fix it")
+            var dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, availability, ERROR_DIALOG_REQUEST)
+            dialog.show()
+        } else {
+            Toast.makeText(this, "We cannot make map request", Toast.LENGTH_SHORT).show()
+        }
+        return false
     }
 
 }
