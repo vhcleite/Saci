79254023646390bdb89aa96ed9d7a7876f1e5da6
Victor Leite <victor_leite@outlook.com.br>
2019-07-28 14:17:59 -0300

    Implementado servico de localizacao do smartphone

--- app/src/main/java/padm/ufabc/edu/saci/MapActivity.kt
+++ app/src/main/java/padm/ufabc/edu/saci/MapActivity.kt
@@ -1,6 +1,7 @@
 package padm.ufabc.edu.saci
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,16 +9,17 @@ import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnCompleteListener
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val DEFAULT_ZOOM = 15f
     private lateinit var mMap: GoogleMap
 
     private val TAG = "MapActitvity"
@@ -25,11 +27,37 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private val COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
     private val LOCATION_PERMISSION_REQUEST_CODE = 1234
     private var mPermissionsLocationGranted = false
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
         getLocationPermission()
+    }
+
+    private fun moveCamera(latlong: LatLng, zoom: Float) {
+        Log.d(TAG, "moveCamera: moving camera to: lat: " + latlong.latitude + " long: " + latlong.longitude)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlong, zoom))
+    }
+
+    private fun getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting current device location")
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if(mPermissionsLocationGranted) {
+            try {
+                fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+                    if(location != null) {
+                        Log.d(TAG, "getDeviceLocation: device location successfully picks up")
+                        moveCamera(LatLng(location.latitude, location.longitude), DEFAULT_ZOOM)
+                    } else {
+                        Toast.makeText(this, "Couldn't catch current location", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } catch (e: SecurityException) {
+                Log.d(TAG, "getDeviceLocation: SecurityException: " + e.message)
+            }
+        }
     }
 
     private fun initMap() {
@@ -49,27 +77,38 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show()
         Log.d(TAG, "The map is ready")
         mMap = googleMap
 
+        if(mPermissionsLocationGranted) {
+            getDeviceLocation()
+            mMap.isMyLocationEnabled = true
+        } else {
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
     }
 
     private fun getLocationPermission() {
         Log.d(TAG, "getLocationPermission: getting location permission")
         val permissions = arrayOf(FINE_LOCATION, COARSE_LOCATION)
 
-        if(ContextCompat.checkSelfPermission(this.applicationContext, FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-            ContextCompat.checkSelfPermission(this.applicationContext, COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if(hasLocationPermission()) {
                 mPermissionsLocationGranted = true
+            initMap()
         } else {
             ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE)
         }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this.applicationContext, FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this.applicationContext, COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
