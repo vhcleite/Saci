ebc74e46fd00d350fcef606853b696124a172ae4
Victor Leite <victor_leite@outlook.com.br>
2019-07-28 23:41:25 -0300

    Colocando título na visualização de incidente

--- app/src/main/java/padm/ufabc/edu/saci/IncidentVisualizationActivityActivity.kt
+++ app/src/main/java/padm/ufabc/edu/saci/IncidentVisualizationActivityActivity.kt
@@ -8,5 +8,6 @@ class IncidentVisualizationActivityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_incident_visualization_activity)
+        setTitle("Enchente na praça")
     }
 }
