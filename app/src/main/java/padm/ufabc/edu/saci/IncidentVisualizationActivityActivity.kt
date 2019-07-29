77c1d760cb05b0266ef8003e4d5eefe5f211abf4
Victor Leite <victor_leite@outlook.com.br>
2019-07-28 23:18:50 -0300

    Criada atividade de visualização de incidente

---
+++ app/src/main/java/padm/ufabc/edu/saci/IncidentVisualizationActivityActivity.kt
@@ -0,0 +1,12 @@
+package padm.ufabc.edu.saci
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IncidentVisualizationActivityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_incident_visualization_activity)
+    }
+}
